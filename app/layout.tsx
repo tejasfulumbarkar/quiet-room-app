924c7fa8e6f15f20e36db45071937cccb92b935e
Tejas Fulumbarkar <tejasshyamfulumbarkar@gmail.com>
2025-12-31 22:19:17 +0530

    Update logo to Quiet Room

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/Qlogo.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/Qlogo.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/Qlogo.png",
+        type: "image/png",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/Qlogo.png",
   },
 }
 
